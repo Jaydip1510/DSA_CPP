48084ae69ae7021eae2d53809f2caee72f99d5ec
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-02 16:05:50 +0530

    first demo

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+   
+    cout << "welcome DSA" << endl;
+    cout << "Enter the Value of A:-";
+    cin >> a;
+
+    cout << "Enter the Value of B:-";
+    cin >> b;
+
+    c = a + b;
+    cout << "Addtion is :-" << c;
+    return 0;
+}
