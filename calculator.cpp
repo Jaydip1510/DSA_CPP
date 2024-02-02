c25b8f09ed45fbd28fb3b62cc58116b4444bf1e7
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-02 16:39:28 +0530

    allfile

---
+++ calculator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      int a,b,c,d,e,f;
+      cout <<"Enter Value of A:-";
+      cin >> a;
+
+      cout <<"Enter Value of B:-";
+      cin >> b;
+      
+      c = a + b;
+      d = a - b;
+      e = a * b;
+      f = a / b;
+
+      cout << "Addition is:-" << c << endl;
+      cout << "Subtraction is:-" << d << endl;
+      cout << "multiplication is:-" << e << endl;
+      cout <<"divisor is:-" << f << endl;
+
+
+      return 0;
+
+      
+}
